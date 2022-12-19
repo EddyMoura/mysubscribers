3be81db20104ee67714dc5d82e70658505a4f291
Eddy <moura@ciandt.com>
2022-12-19 16:00:54 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/mysubscribers/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mysubscribers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.mysubscribers.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+    }
+}
